8cb8db91579cfcd16d77163987fb55d3f66aea89
AdinotAadi <adi.notaadi@gmail.com>
2024-03-08 12:52:12 +0530

    feat: add preloader

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import { useEffect, useState } from "react";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
 import Info from "./components/Info";
 import NavBar from "./components/NavBar";
 import Speakers from "./components/Speakers";
+import Loader from "./components/Loader";
 
 function App() {
-	return (
+	const [isLoading, setIsLoading] = useState(true);
+	useEffect(() => {
+		const loading = () => {
+			setTimeout(() => {
+				setIsLoading(false);
+			}, 3000);
+		};
+		loading();
+	}, []);
+	return isLoading ? (
+		<Loader />
+	) : (
 		<div>
 			<NavBar />
 			<Home />
