315aff5fed0d767662e60677dbc3277f1e49fd42
AdinotAadi <adi.notaadi@gmail.com>
2024-03-08 21:10:50 +0530

    feat: add link to purchase tickets

--- src/components/Tickets.jsx
+++ src/components/Tickets.jsx
@@ -10,29 +10,34 @@ const Tickets = () => {
 					</h2>
 
 					<p class="sm:block text-white mt-4">
-						Watch out this section in the future to reserve your tickets for the
-						event.
+						Head over to the link below to reserve your seats before all the tickets
+						are sold out!
 					</p>
 				</div>
 
 				<div class="mx-auto mt-8 max-w-xl">
-					<button class="group mt-4 mx-auto flex w-full items-center justify-center gap-2 rounded-md bg-red-500 px-5 py-3 text-white transition sm:mt-0 sm:w-auto hover:bg-gray-800 hover:text-red-500 hover:duration-300 ticketButton">
-						<span class="font-medium"> Beginning reservations soon! </span>
-
-						<svg
-							class="size-5 rtl:rotate-180"
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke="currentColor">
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="2"
-								d="M17 8l4 4m0 0l-4 4m4-4H3"
-							/>
-						</svg>
-					</button>
+					<a
+						className=""
+						href="https://linktr.ee/tedxxie24"
+						target="_blank"
+						rel="noreferrer">
+						<div class="group mt-4 mx-auto flex w-full items-center justify-center gap-2 rounded-md bg-red-500 px-5 py-3 text-white transition sm:mt-0 sm:w-auto hover:bg-gray-800 hover:text-red-500 hover:duration-300 ticketButton">
+							<span class="font-medium">Buy tickets</span>{" "}
+							<svg
+								class="size-5 rtl:rotate-180"
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke="currentColor">
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									stroke-width="2"
+									d="M17 8l4 4m0 0l-4 4m4-4H3"
+								/>
+							</svg>
+						</div>
+					</a>
 				</div>
 			</div>
 		</section>
